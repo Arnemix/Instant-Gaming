5fa5b43c80bc78a1be836b116d75a06ec46ea6bd
Arnemix <amindrayef@gmail.com>
2024-03-01 10:35:06 +0100

    Développement de la page détails des jeux

--- pages/games/[slug].jsx
+++ pages/games/[slug].jsx
@@ -2,6 +2,12 @@ import Loader from "@/components/loader/Loader";
 import NavBar from "@/components/navbar/NavBar";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { BsNintendoSwitch } from "react-icons/bs";
+import { FaExternalLinkAlt, FaPlaystation, FaXbox } from "react-icons/fa";
+import { FaComputer } from "react-icons/fa6";
+import "@/app/globals.css";
+import "./style.scss";
+import Link from "next/link";
 
 // Note : régler le problème de CSS dans la navbar quand elle est appelée dans ce composant
 
@@ -11,6 +17,18 @@ function GameDetails() {
     const [games, setGames] = useState([]);
     const [game, setGame] = useState({});
     const router = useRouter();
+
+    const getPlateforme = (game) => {
+        if (game.platform === "PC (Windows)") {
+            return <FaComputer style={{ scale: "1.3" }} />;
+        } else if (game.platform === "PS4") {
+            return <FaPlaystation />;
+        } else if (game.platform === "Xbox") {
+            return <FaXbox />;
+        } else {
+            return <BsNintendoSwitch />;
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -46,7 +64,38 @@ function GameDetails() {
         <>
             <NavBar />
             <div className="game-details-container">
-                <div className="game-details-title"></div>
+                <div className="game-details-image">
+                    <img src={game.thumbnail} alt={game.title} />
+                </div>
+                <div className="game-details">
+                    <div className="game-title">
+                        <h1>{game.title}</h1>
+                    </div>
+                    <div className="game-infos">
+                        <h3>{game.short_description}</h3>
+                        <p>
+                            <span>Date de sortie</span> : {game.release_date}
+                        </p>
+                        <p>
+                            <span>Genre</span> : {game.genre}
+                        </p>
+                        <p>
+                            <span>Editeur</span> : {game.publisher}
+                        </p>
+                        <p className="game-infos-platform">
+                            <span>Plateforme</span> : {getPlateforme(game)}
+                        </p>
+                        <p>
+                            <span>Site officiel</span> :{" "}
+                            <a href={game.game_url}>
+                                <FaExternalLinkAlt style={{ color: "rgb(254, 64, 32)" }} />
+                            </a>
+                        </p>
+                    </div>
+                    <div className="game-options">
+                        <button className="button">Ajouter au panier {Math.floor(Math.random() * 100)}€</button>
+                    </div>
+                </div>
             </div>
         </>
     );
