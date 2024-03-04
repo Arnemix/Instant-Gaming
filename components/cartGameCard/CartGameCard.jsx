8e5db51ec1194e936f809ed498ca5879ec72fca6
Arnemix <amindrayef@gmail.com>
2024-03-04 11:55:25 +0100

    Avancement du responsive : navbar & games-container

--- components/cartGameCard/CartGameCard.jsx
+++ components/cartGameCard/CartGameCard.jsx
@@ -10,13 +10,15 @@ function CartGameCard({ game }) {
                 <img src={game.thumbnail} alt={game.title} />
             </div>
             <div className="cart-card-content">
-                <div className="card-card-info">
+                <div className="cart-card-info">
                     <h1>{game.title}</h1>
                     <h3>{game.price}€</h3>
                     <p>Quantité : {game.quantity}</p>
                 </div>
                 <div className="cart-card-button">
-                    <button className="button remove-item-button" onClick={() => dispatch(removeFromCard(game))}>Supprimer</button>
+                    <button className="button cart-item-button" onClick={() => dispatch(removeFromCard(game))}>
+                        Supprimer
+                    </button>
                 </div>
             </div>
         </div>
--- pages/panier/index.jsx
+++ pages/panier/index.jsx
@@ -1,9 +1,9 @@
-import NavBar from "@/components/navbar/NavBar";
-import React, { useEffect } from "react";
 import "@/app/globals.css";
-import { useSelector } from "react-redux";
-import Loader from "@/components/loader/Loader";
 import CartGameCard from "@/components/cartGameCard/CartGameCard";
+import NavBar from "@/components/navbar/NavBar";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 import "./style.scss";
 
 function CartPage(props) {
@@ -14,6 +14,7 @@ function CartPage(props) {
     }, [cart]);
     return (
         <>
+            <Toaster position="bottom-left" />
             <NavBar />
             <div className="page-container">
                 <h1 className="page-title">Mon panier</h1>
@@ -25,6 +26,18 @@ function CartPage(props) {
                             return <CartGameCard key={index} game={item} />;
                         })
                     )}
+                </div>
+                <div className="cart-summary">
+                    <h2>Total : {cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}€</h2>
+                    <h2>TVA : {cart.reduce((acc, item) => acc + item.price * item.quantity * 0.21, 0)}€</h2>
+                    <button
+                        onClick={() => {
+                            toast.error("Les moyens de paiements ne sont pas encore disponibles");
+                        }}
+                        className="button cart-item-button"
+                    >
+                        Procéder au paiement
+                    </button>
                 </div>
             </div>
         </>
